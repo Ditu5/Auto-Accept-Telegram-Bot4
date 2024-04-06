4b9c1c62bb584436e21c4e059c606d8feb84b66d
Snowball <162016877+Snowball-01@users.noreply.github.com>
2024-04-06 21:29:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,10 @@
-import asyncio
 import logging
 import logging.config
-import warnings
 from pyrogram import Client
 from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
-from pytz import timezone
-from datetime import datetime
 from plugins.web_support import web_server
-from plugins.admin_panel import user
 import pyromod
 
 
@@ -49,38 +43,10 @@ class Bot(Client):
             except:
                 pass
 
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")
-            except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
-
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot Stopped 🙄")
 
 
-bot_instance = Bot()
-
-def main():
-    async def start_services():
-        if Config.SESSION:
-            await asyncio.gather(
-                user.start(),        # Start the Pyrogram Client
-                bot_instance.start()  # Start the bot instance
-            )
-        else:
-            await asyncio.gather(
-                bot_instance.start()
-            )
-        
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_services())
-    loop.run_forever()
-
-if __name__ == "__main__":
-    warnings.filterwarnings("ignore", message="There is no current event loop")
-    main()
+bot = Bot()
+bot.run()
